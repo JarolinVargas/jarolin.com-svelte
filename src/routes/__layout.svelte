dc278ba3b91bee133f82f386821c66a9a9f2f9a5
Jarolin Vargas <jarolinvargas@gmail.com>
2021-11-20 20:34:27 -0500

    frame layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,134 @@
+<script>
+    import '../frame.css';
+</script>
+
+<div class="root">
+    <div class="frame">
+        <nav>
+            <div class="active-tab-indicator"></div>
+            <ul>
+                <li class="navitem-separator"></li>
+                <li><a href="/">About Me</a></li>
+                <li class="navitem-separator"></li>
+                <li><a href="/portfolio">Portfolio</a></li>
+                <li class="navitem-separator"></li>
+                <li><a href="/writings">Writings</a></li>
+                <li class="navitem-separator"></li>
+                <li><a href="/contact">Contact</a></li>
+                <li class="navitem-separator"></li>
+            </ul>
+        </nav>
+        <main class="content">
+            <slot/>
+        </main>
+    </div>
+</div>
+
+<style>
+    a {
+        text-transform: uppercase;
+        color: #FFF;
+    }
+
+    .root {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        vertical-align: middle;
+        background-image: url('../src/assets/diagonal-line.svg');
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+
+    .frame {
+        width: 1300px;
+        height: 725px;
+        max-width: 100%;
+        max-height: 100%;
+        margin: auto;
+        padding: 30px 15px;
+        font-size: 16px;
+        color: rgba(45,51,86,.7490196078431373);
+        position: relative;
+        box-sizing: border-box;
+    }
+
+    main {
+        width: 100%;
+        height: 100%;
+        border: 1px solid;
+        border-image-slice: 1;
+        border-top: 0;
+        z-index: 1;
+        position: relative;
+        box-sizing: border-box;
+        border-image-source: linear-gradient(to right, rgb(58, 244, 245), rgb(22, 77, 254));
+    }
+
+    nav {
+        width: 100%;
+        height: 50px;
+        width: calc(100% - 30px);
+        position: absolute;
+        margin-top: -26px;
+        display: flex;
+        z-index: 2;
+        font-size: 1.2em;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    ul {
+        display: flex;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        overflow: hidden;
+    }
+
+    li {
+        display: inline-block;
+        align-self: center;
+        text-align: center;
+        white-space: nowrap;
+        font-weight: 500;
+    }
+
+    li:not(.navitem-separator) {
+        padding: 0 10px;
+        width: -webkit-fit-content;
+        width: -moz-fit-content;
+        width: fit-content;
+    }
+
+    .navitem-separator {
+        height: 1px;
+        flex-basis: 30px;
+        min-width: 30px;
+        margin-top: 1px;
+        background-image: linear-gradient(to right, rgb(58, 244, 245), rgb(22, 77, 254));
+        background-attachment: fixed;
+        mix-blend-mode: difference;
+    }
+
+    .navitem-separator:last-child {
+        flex-basis: 100%;
+    }
+    
+    .active-tab-indicator {
+        left: 85px;
+        background-image: linear-gradient(to right, rgb(58, 244, 245), rgb(22, 77, 254));
+        width: 6px;
+        height: 6px;
+        border-radius: 100%;
+        background-attachment: fixed;
+        mix-blend-mode: difference;
+        position: absolute;
+        transition-property: left;
+        transition-duration: .3s;
+        transition-timing-function: ease-out;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../frame.css';
+</script>
+
+<svelte:head>
+    <title>Jarolin Vargas - Front-end Engineer &amp; UI/UX Designer</title>
+</svelte:head>
